ce5af3fa4473f0ca32e5a0d513c305fd020a9f1b
yar1kukharenko <116860258+berryblacka@users.noreply.github.com>
2023-03-31 09:35:40 +0300

    Fix

--- scripts/script.js
+++ scripts/script.js
@@ -19,11 +19,24 @@ const corals__position_bottom_right = document.getElementById('corals__position_
 const circle__size_small_1 = document.getElementById('circle__size_small_1');
 const circle__size_small_2 = document.getElementById('circle__size_small_2');
 const circle__size_small_3 = document.getElementById('circle__size_small_3');
+const text__top_left = document.getElementById('text__top_left');
+const text__top_right = document.getElementById('text__top_right');
+const text__middle_middle = document.getElementById('text__middle_middle');
+const text__middle_left = document.getElementById('text__middle_left');
+const text__middle_right = document.getElementById('text__middle_right');
+const text__bottom_middle = document.getElementById('text__bottom_middle');
+const text__footer_left = document.getElementById('text__footer_left');
+const text__footer_right = document.getElementById('text__footer_right');
+const circle__size_big_1 = document.getElementById('circle__size_big_1');
+const circle__size_big_2 = document.getElementById('circle__size_big_2');
+const circle__size_medium_1 = document.getElementById('circle__size_medium_1');
+const circle__size_medium_2 = document.getElementById('circle__size_medium_2');
+const circle__size_medium_3 = document.getElementById('circle__size_medium_3');
 
 // .....Функция для перетягивания......
 const Dragger = (elem) => {
   elem.style.cursor = 'pointer';
-  elem.onmousedown = function (event) {
+  elem.onmousedown = (event) => {
     const shiftX = event.clientX - elem.getBoundingClientRect().left;
     const shiftY = event.clientY - elem.getBoundingClientRect().top;
     elem.style.position = 'absolute';
@@ -40,21 +53,20 @@ const Dragger = (elem) => {
       moveAt(event.pageX, event.pageY);
     }
     document.addEventListener('mousemove', onMouseMove);
-    elem.onmouseup = function () {
+    elem.onmouseup = () => {
       document.removeEventListener('mousemove', onMouseMove);
       elem.onmouseup = null;
     };
   };
 
-  elem.ondragstart = function () {
-    return false;
-  };
+  elem.ondragstart = () => false;
 };
 
 // ..........функция для уменьшения прозрачности...............
 
 function OpacityChanger(svgElement) {
   let opacity = 1;
+  svgElement.style.transition = 'opacity 1s';
   svgElement.style.cursor = 'pointer';
   svgElement.addEventListener('click', () => {
     opacity *= 0.6;
@@ -72,6 +84,7 @@ function OpacityChanger(svgElement) {
 function SizeSmaller(svgElement) {
   let scale = 1;
   svgElement.style.cursor = 'pointer';
+  svgElement.style.transition = 'transform 1s';
   svgElement.addEventListener('click', () => {
     scale *= 0.6;
     // eslint-disable-next-line no-param-reassign
@@ -83,10 +96,17 @@ function SizeSmaller(svgElement) {
   });
 }
 
+const Disappear = (elem) => {
+  elem.style.cursor = 'pointer';
+  elem.addEventListener('click', () => {
+    elem.style.display = 'none';
+  });
+};
+
 SizeSmaller(wave__position_top_left);
 OpacityChanger(rainbow__position_bottom_right);
 SizeSmaller(radial__position_top_right);
-Dragger(radial__position_middle_left);
+SizeSmaller(radial__position_middle_left);
 Dragger(gradient__position_top_middle);
 OpacityChanger(corals__position_top_left);
 SizeSmaller(radial__position_bottom_left);
@@ -101,3 +121,16 @@ Dragger(rainbow__position_bottom_middle);
 Dragger(circle__size_small_1);
 Dragger(circle__size_small_2);
 Dragger(circle__size_small_3);
+Dragger(circle__size_big_1);
+Dragger(circle__size_big_2);
+Dragger(circle__size_medium_1);
+Dragger(circle__size_medium_2);
+Dragger(circle__size_medium_3);
+Disappear(text__top_left);
+Disappear(text__top_right);
+Disappear(text__middle_middle);
+Disappear(text__middle_left);
+Disappear(text__middle_right);
+Disappear(text__bottom_middle);
+Disappear(text__footer_left);
+Disappear(text__footer_right);
